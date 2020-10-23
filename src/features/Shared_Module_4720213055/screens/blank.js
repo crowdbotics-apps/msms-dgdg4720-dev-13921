3fcee4c53522c15b1ecfeeaeb4f1c37c810e56ec
Crowdbotics <team@crowdbotics.com>
2020-10-23 06:51:33 +0000

    add component Shared_Module_4720

---
+++ src/features/Shared_Module_4720213055/screens/blank.js
@@ -0,0 +1,63 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_5: "" }
+
+  render = () => (
+    <View>
+      <Text>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+      <Button
+        title="Press Here..!!"
+        color="#33ff41"
+        onPress={() => alert("Pressed!")}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16
+  },
+
+  View_1: {},
+  Text_3: {},
+  TextInput_5: {},
+  Button_7: {},
+  View_1: {},
+  Text_3: {},
+  TextInput_5: {},
+  Button_7: {},
+  View_1: {},
+  Text_3: {},
+  TextInput_5: {},
+  Button_7: {}
+})
